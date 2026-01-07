cf58133240be63415ea9008ef392040f9b9278f1
amzdudes <amzdudes.ai02@gmail.com>
2026-01-07 13:33:49 +0500

    Fix favicon configuration - remove manual head tag, use Next.js metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -25,10 +25,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/amz-favicons.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/amz-favicons.png" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
